eec256a78d209fb684e0092d8b0c29f2d363f56b
Philip Simonson <psimonson1988@gmail.com>
2020-08-26 19:25:10 -0400

    Moved code for pluginmanager to seperate directory.

---
+++ src/plugin_manager.h
@@ -0,0 +1,20 @@
+#ifndef _PLUGIN_MANAGER_H_
+#define _PLUGIN_MANAGER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct plugin_manager plugin_manager_t;
+
+int PluginManager_init(const char *dirname);
+int PluginManager_exec(size_t id);
+void PluginManager_print(void);
+void PluginManager_destroy(void);
+const char *PluginManager_getName(plugin_manager_t *pm);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
